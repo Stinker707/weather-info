aadbba13554ffca432799e135a4e7644688d499e
Oleg Ignatev <karambas707@gmail.com>
2024-11-24 11:31:19 +0300

    вынес карточку в index.html

--- main.js
+++ main.js
@@ -4,43 +4,31 @@ import conditions from './condition.js';
 const apiKey = 'e6051476c5664333a9f172047242011';
 const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
-const main = document.querySelector('main')
+const card = document.querySelector('#card');
 
 async function getWeather(city) {
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-    console.log(data.error);
     return data;
 }
 
-
-function removeCard() {
-    const prevCard = document.querySelector('.card');
-    if (prevCard) {
-        prevCard.remove();
-     }
-}
-
 function showError(errorMessage) {
-    const html = `<div class="card">${errorMessage}</div>`;
-    main.innerHTML = html;
+    const html = `${errorMessage}`;
+    console.log(card)
+    card.innerHTML = html;
 }
 
 function showCard({name, country, temp, condition, imgPath}) {
     const htmlImg = imgPath ? `<img class="card-img" src="${imgPath}" alt=""></img>` : ''
-    const html = `<div class="card">
-                        <h2 class="card-city">${name} <span>${country}</span></h2>
+    const html = `<h2 class="card-city">${name} <span>${country}</span></h2>
+        <div class="card-weather">
+            <div class="card-value">${temp}<sup>°c</sup></div>
+            <div>${htmlImg}</div>
+            </div class="card-description">${condition}</div>
+        </div>`
 
-                        <div class="card-weather">
-                            <div class="card-value">${temp}<sup>°c</sup></div>
-                            <div>${htmlImg}</div>
-                            </div class="card-description">${condition}</div>
-                        </div>
-                    </div>`
-
-    main.innerHTML = html;
+    card.innerHTML = html;
 }
 
 async function checkImgSrc(url) {
@@ -67,14 +55,8 @@ form.onsubmit = async function (e) {
     const data = await getWeather(city);
 
     if(data.error) {
-        removeCard();
         showError(data.error.message);
     } else {
-
-        removeCard();
-
-        console.log(data.current.condition.code);
-
         const info = conditions.find((obj) => obj.code === data.current.condition.code)
         
         const filePath = './img/' + (data.current.is_day ? 'day' : 'night') + '/';
